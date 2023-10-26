3677639ac46f79b08d48759342cf28108d6ac24f
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-10-26 12:20:40 +0530

    save to db

--- users/views.py
+++ users/views.py
@@ -10,6 +10,7 @@ def register(request):
     if request.method=='POST':
         form=UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username=form.cleaned_data.get('username')
             messages.success(request, f'Account successfully created for {username}!')
             return redirect('blog-home')
