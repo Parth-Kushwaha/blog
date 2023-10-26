80005d318927c66f1e5895d0a67982ee230e9379
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-10-26 13:47:03 +0530

    login logout

--- blog/urls.py
+++ blog/urls.py
@@ -20,7 +20,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('', include('blog_app.urls')),
     path('', include('users.urls')),
-    path('login/', auth_views.LoginView.as_view('users/login.html'),name='login')
+
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('login/', auth_views.LogoutView.as_view(template_name='users/login.html'),name='logout')
 ]
